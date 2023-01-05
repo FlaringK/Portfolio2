e3bed80df89017710ac063597b065e50e7e05a9d
FlaringK <caelans@iinet.net.au>
2023-01-05 17:08:11 +0800

    Gallery

--- script.js
+++ script.js
@@ -18,16 +18,14 @@ document.addEventListener('scroll', function(e) {
 });
 
 /* Add line numbers */
-document.querySelectorAll(".window").forEach(e => {
+document.querySelectorAll(".notes").forEach(e => {
   e.querySelectorAll("p").forEach((p, i) => {
     p.style.setProperty("--lineNumber", `"${i+1}"`)
   })
 })
 
-// Load json
-fetch("./projects.json")
-  .then(response => response.json())
-  .then(json => loadProjects(json))
+// Load Project json
+fetch("./projects.json").then(response => response.json()).then(json => loadProjects(json))
 
 const fileMenu = document.getElementById("projectMenu")
 const preview = document.getElementById("projectPreview")
@@ -42,11 +40,36 @@ let loadProjects = projects => {
     icon.className = "icon"
     icon.innerHTML = `<div class="iconimg"> <img src="${data.icon}"> </div> ${data.title}`
 
-    icon.onclick = () => {
-      preview.innerHTML = `<div class="previewimg"> <a href='${data.link}'><img src="${data.icon}"> </div> <h2>${data.title}</h2></a> ${data.description}`
+    icon.onclick = e => {
+      preview.innerHTML = `<a href='${data.link}'><div class="previewimg"> <img src="${data.icon}"> </div> <h2>${data.title}</h2></a> ${data.description}`
+
+      const icons = document.querySelectorAll("#projectMenu .icon")
+      icons.forEach(i => { i.className = "icon" })
+      e.target.className = "icon active"
     }
 
     fileMenu.append(icon)
 
   })
+}
+
+// Load Art json
+fetch("./artworks.json").then(response => response.json()).then(json => loadArtworks(json))
+
+const artList = document.getElementById("artList")
+const viewerImg = document.querySelector("#artViewer img")
+
+let loadArtworks = artworks => {
+
+  artworks.forEach(work => {
+    let img = document.createElement("img")
+    img.src = work
+
+    img.onclick = e => {
+      viewerImg.src = e.target.src
+    }
+
+    artList.append(img)
+  })
+
 }
